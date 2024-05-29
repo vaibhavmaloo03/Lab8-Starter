5eeeaf25621e4560d86033fa62aa6390ca1a996f
vaibhavmaloo03 <vmaloo@ucsd.edu>
2024-05-28 22:54:28 -0700

    lab 8 changes

--- sw.js
+++ sw.js
@@ -1,40 +1,41 @@
-// sw.js - This file needs to be in the root of the directory to work,
-//         so do not move it next to the other scripts
+// sw.js - This script must reside at the root of the directory to function properly
+const CACHE_STORAGE_NAME = 'lab-8-cache';
 
-const CACHE_NAME = 'lab-8-starter';
+// Install the service worker and pre-cache the specified resources
+self.addEventListener('install', (event) => {
+  const preCacheUrls = [
+    'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+    'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+  ];
 
-// Installs the service worker. Feed it some initial URLs to cache
-self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+    caches.open(CACHE_STORAGE_NAME).then((cache) => {
+      // Cache the predefined URLs during the `installation
+      return cache.addAll(preCacheUrls);
     })
   );
 });
 
-// Activates the service worker
-self.addEventListener('activate', function (event) {
+// Activate the service worker and take control of the client
+self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-// Intercept fetch requests and cache them
-self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+// Intercept network requests and serve them from the cache if available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open(CACHE_STORAGE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        // Return cached response if available, otherwise fetch from the network
+        return cachedResponse || fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
 });
